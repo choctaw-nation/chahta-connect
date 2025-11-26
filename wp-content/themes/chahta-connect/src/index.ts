5f49a4390696e9854aae8eaa998f7f64a076c365
jnwade92 <125072842+jnwade92@users.noreply.github.com>
2025-11-26 15:40:13 -0600

    Implement smooth scrolling for anchor links; update footer and header… (#12)
    * Implement smooth scrolling for anchor links; update footer and header logo sizes; adjust positioning for best value label; enhance Gravity Forms button styles and add spinner animations
    
    * Remove unused spinner styles and button color overrides from Gravity Forms SCSS

--- wp-content/themes/chahta-connect/src/index.ts
+++ wp-content/themes/chahta-connect/src/index.ts
@@ -1,1 +1,31 @@
 import './styles/main.scss';
+const header = document.getElementById( 'site-header' );
+const pageNavButtons =
+	document.querySelectorAll< HTMLAnchorElement >( 'a[href^="#"]' );
+
+pageNavButtons.forEach( ( button ) => {
+	const href = button.getAttribute( 'href' );
+	if ( ! href || href === '#' ) {
+		return;
+	}
+
+	button.addEventListener( 'click', ( event ) => {
+		event.preventDefault();
+
+		const target = document.getElementById( href.replace( '#', '' ) );
+
+		if ( ! target ) {
+			return;
+		}
+
+		const offset = header ? header.offsetHeight : 0;
+		const targetPosition =
+			target.getBoundingClientRect().top + window.scrollY;
+		const startPosition = window.scrollY;
+		const distance = targetPosition - startPosition - offset;
+		window.scrollTo( {
+			top: startPosition + distance,
+			behavior: 'smooth',
+		} );
+	} );
+} );
